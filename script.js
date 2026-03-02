af609ad95bfb76ff5faa1402a575a50ebb6e2831
okumu87 <okumu793@gmail.com>
2026-03-02 11:33:25 +0100

    promises

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// Basic JavaScript starter
 
-console.log('JavaScript is running');
-
-// Add your code below
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+.then(res => res.json())
+.then(data => {
+     console.log(data)
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.getElementById('author').textContent = `By: ${data.user.name}`
+})
