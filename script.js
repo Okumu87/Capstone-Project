10bb3d5eb0e19bf94d00399503198d4165a99f0a
okumu87 <okumu793@gmail.com>
2026-03-03 20:40:10 +0100

    added author

--- script.js
+++ script.js
@@ -9,4 +9,6 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 .catch(err => {
     console.log(err)
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1506744038136-46273834b3fb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDEwfHx1bmNsZWFyfGVufDB8fHx8MTY4NjQ5ODk0NA&ixlib=rb-4.0.3&q=80&w=1080)`
+    document.getElementById('author').textContent = `By: Dodie Clark`
 })
+
